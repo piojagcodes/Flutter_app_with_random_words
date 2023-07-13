ff712f727f21ed1b10d10d8efe7582e1b40f3093
piojagcodes <piojag@gmail.com>
2023-07-13 21:29:12 +0200

    Saving WordPairs with Navigation widget

--- my_flutter_app/lib/random_words.dart
+++ my_flutter_app/lib/random_words.dart
@@ -46,7 +46,23 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
-  void _pushSaved() {}
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+            title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)));
+      });
+
+      final List<Widget> divided = ListTile.divideTiles(
+        context: context,
+        tiles: tiles,
+      ).toList();
+
+      return Scaffold(
+          appBar: AppBar(title: Text('Saved WordPairs')),
+          body: ListView(children: divided));
+    }));
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
